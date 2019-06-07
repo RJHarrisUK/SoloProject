0000dba1c5ed332e2bf6e003891bd8780686a4d3
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-07 10:29:43 +0100

    initial commit

---
+++ src/main/java/com/bae/APIDevAssessment/App.java
@@ -0,0 +1,13 @@
+package com.bae.APIDevAssessment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
