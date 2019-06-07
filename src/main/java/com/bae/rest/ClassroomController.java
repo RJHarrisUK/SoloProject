ba3f7895add4ad3ea8d7fb14da681a1e604e6e27
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-07 12:09:12 +0100

    added rest layer and endpoints

---
+++ src/main/java/com/bae/rest/ClassroomController.java
@@ -0,0 +1,22 @@
+package com.bae.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.bae.business.ClassroomService;
+
+@Path("/classroom")
+public class ClassroomController {
+
+	@Inject
+	private ClassroomService service;
+	
+	@Path("/getAllClassrooms")
+	@GET
+	@Produces({"application/json"})
+	public String getAllClassrooms() {
+		return service.getAllClassrooms();
+	}
+}
---
+++ src/main/java/com/bae/rest/JAXActivator.java
@@ -0,0 +1,9 @@
+package com.bae.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+
+public class JAXActivator extends Application {
+}
