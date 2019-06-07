1330b82d7d094e82b7b7ed64294df9321061fae9
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-07 15:12:47 +0100

    added update from Trainee throughout layers. Same unresolved issue as Classroom. com.fasterxml.jackson.databind.JsonMappingException: Can not deserialize instance of int out of START_OBJECT token  at [Source: io.undertow.servlet.spec.ServletInputStreamImpl@5a93c9ea; line: 1, column: 5]

--- src/main/java/com/bae/persistence/repository/TraineeMapRepository.java
+++ src/main/java/com/bae/persistence/repository/TraineeMapRepository.java
@@ -3,6 +3,8 @@ package com.bae.persistence.repository;
 import java.util.Map;
 import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
+
+import com.bae.persistence.domain.Classroom;
 import com.bae.persistence.domain.Trainee;
 import com.bae.util.JSONUtil;
 
@@ -30,6 +32,17 @@ public class TraineeMapRepository implements TraineeRepository {
 	public String deleteTrainee(int traineeID) {
 		traineeMap.remove(traineeID);
 		return "Trainee successfully deleted";
+	}
+
+	@Override
+	public String updateTrainee(int traineeID, String trainee) {
+		Trainee upTrainee = util.getObjectForJSON(trainee, Trainee.class);
+		
+		if (traineeMap.containsKey(traineeID)) {
+			traineeMap.replace(traineeID, upTrainee);
+			return "Trainee added" + util.getJSONForObject(traineeMap.values());
+		}
+		return "Cannot update this trainee.";
 	}
 
 
--- src/main/java/com/bae/rest/TraineeController.java
+++ src/main/java/com/bae/rest/TraineeController.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -32,5 +33,11 @@ public class TraineeController {
 	@Produces({" application/json "})
 	public String deleteTrainee(@PathParam("traineeID") int traineeID) {
 		return service.deleteTrainee(traineeID);
-}
+	}
+	@Path("/updateTrainee/{traineeID}")
+	@PUT
+	@Produces({" application/json "})
+	public String updateTrainee(@PathParam("traineeID") String trainee, int traineeID) {
+		return service.updateTrainee(trainee, traineeID);
+	}
 }
