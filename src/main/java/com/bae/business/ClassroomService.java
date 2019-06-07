8909998166a4b534289935ac12c753d420ab77d9
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-07 12:02:56 +0100

    added businessLayer

---
+++ src/main/java/com/bae/business/ClassroomService.java
@@ -0,0 +1,13 @@
+package com.bae.business;
+
+public interface ClassroomService {
+
+//	//C
+//	String createClassroom(String classroom);
+	//R
+	String getAllClassrooms();
+//	//U
+//	String updateClassroom(String classroom, int id);
+//	//D
+//	String deleteClassroom(int id);
+}
