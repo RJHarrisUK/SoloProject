95aa8ab8ad1acacd3a28556952b4ee904d47ab24
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-07 10:52:45 +0100

    developed structure and test deployed success

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO MOVIE (id, title, ageRating) VALUES (1, 'Saw', '18')
+INSERT INTO MOVIE (id, title, ageRating) VALUES (2, 'Up', 'U')
+INSERT INTO MOVIE (id, title, ageRating) VALUES (3, 'Hop', 'U')
+INSERT INTO MOVIE (id, title, ageRating) VALUES (4, 'Some Film', 'PG')
+
+INSERT INTO SUBMOVIE (id, title, ageRating, subAttribute) VALUES (4, 'Some Film', 'PG', 'Sub')
