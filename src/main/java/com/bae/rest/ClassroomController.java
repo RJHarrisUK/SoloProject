a7eb02a61a2aa3bc373261dac89a3b53c09366ab
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-12 13:53:05 +0100

    fixed update, now fully CRUD functional

--- src/main/java/com/bae/rest/ClassroomController.java
+++ src/main/java/com/bae/rest/ClassroomController.java
@@ -16,43 +16,43 @@ public class ClassroomController {
 
 	@Inject
 	private ClassroomService service;
-	
+
 	// READ
 	@Path("/getAllClassrooms")
 	@GET
-	@Produces({" application/json "})
+	@Produces({ " application/json " })
 	public String getAllClassrooms() {
 		return service.getAllClassrooms();
 	}
-	
+
 	// CREATE
 	@Path("/createClassroom")
 	@POST
-	@Produces({" application/json "})
+	@Produces({ " application/json " })
 	public String createClassroom(String classroom) {
 		return service.createClassroom(classroom);
 	}
-	
+
 	// DELETE
 	@Path("/deleteClassroom/{id}")
 	@DELETE
-	@Produces({"application/json"})
+	@Produces({ "application/json" })
 	public String deleteClassroom(@PathParam("id") int id) {
 		return service.deleteClassroom(id);
-}
-	
+	}
+
 	// UPDATE - unresolved
 	@Path("/updateClassroom/{id}")
 	@PUT
-	@Produces({"application/json"})
-	public String updateClassroom(@PathParam("id") String classroom, int id) {
+	@Produces({ "application/json" })
+	public String updateClassroom(@PathParam("id") int id, String classroom) {
 		return service.updateClassroom(classroom, id);
 	}
-	
+
 	// READ
 	@Path("/findClassroom/{id}")
 	@GET
-	@Produces({"application/json"})
+	@Produces({ "application/json" })
 	public String findClassroom(@PathParam("id") int id) {
 		return service.findClassroom(id);
 	}
